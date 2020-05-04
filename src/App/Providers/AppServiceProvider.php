f8db63ae9c681dc407d272defce57289712e5d7e
bobimicroweber <bobi@microweber.com>
2020-05-04 05:49:34 -0700

    update

--- src/App/Providers/AppServiceProvider.php
+++ src/App/Providers/AppServiceProvider.php
@@ -300,7 +300,7 @@ class AppServiceProvider extends ServiceProvider {
         if (mw_is_installed()) {
             load_all_functions_files_for_modules();
 
-            $this->commands('Microweber\App\Commands\OptionCommand');
+            $this->commands('Microweber\App\Console\Commands\OptionCommand');
 
             $language = get_option('language', 'website');
 
@@ -310,15 +310,15 @@ class AppServiceProvider extends ServiceProvider {
 
             if (is_cli()) {
 
-                $this->commands('Microweber\App\Commands\ResetCommand');
-                $this->commands('Microweber\App\Commands\UpdateCommand');
-                $this->commands('Microweber\App\Commands\ModuleCommand');
-                $this->commands('Microweber\App\Commands\PackageInstallCommand');
+                $this->commands('Microweber\App\Console\Commands\ResetCommand');
+                $this->commands('Microweber\App\Console\Commands\UpdateCommand');
+                $this->commands('Microweber\App\Console\Commands\ModuleCommand');
+                $this->commands('Microweber\App\Console\Commands\PackageInstallCommand');
 
             }
         } else {
             // Otherwise register the install command
-            $this->commands('Microweber\App\Commands\InstallCommand');
+            $this->commands('Microweber\App\Console\Commands\InstallCommand');
         }
 
         $this->loadRoutes();
